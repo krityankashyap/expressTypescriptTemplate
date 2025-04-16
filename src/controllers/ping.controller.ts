61dd90c80c8d1d567e65f5a99a9f15cdfaa9af75
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2025-04-16 18:52:11 +0530

    build-in error in express for controllers

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -1,5 +1,19 @@
-import {Request , Response} from "express";
+import {NextFunction, Request , Response} from "express";
+import fs from "fs";
 
-export const pingHandler = (req : Request , res : Response) => {
-    res.send('Pong');
+export const pingHandler =  (req : Request , res : Response , next : NextFunction)  => {
+
+   fs.readFile("sample" , (err , data) => {
+    if(err){
+        console.log("Error reading file ", err)
+        next(err); // Pass the error to the next middleware which is the default error handler
+    }
+
+   // console.log(data.toString());
+   })
+  
+    // res.status(200).json({
+    //     message : "Pong",
+    //     success : true,
+    // });
 }
